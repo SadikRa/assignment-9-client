138f9a8f89476e94e9a08dfa249b4b74d471ff5e
trrabby1@gmail.com <110276063+trrabby@users.noreply.github.com>
2025-05-10 14:31:25 +0600

    added review

--- src/app/(WithDashboardLayout)/dashboard/admin/reviews/page.tsx
+++ src/app/(WithDashboardLayout)/dashboard/admin/reviews/page.tsx
@@ -1,7 +1,15 @@
+import ManageReviewTable from "@/components/modules/dashboard/dashboard-review";
+import { getAllReviews } from "@/services/Review";
 import React from "react";
 
-const reviewComment = () => {
-  return <div>Review Comment Page</div>;
+const reviewComment = async () => {
+  const allReviews = await getAllReviews();
+  console.log(allReviews.data);
+  return (
+    <div>
+      <ManageReviewTable reviews={allReviews.data} />
+    </div>
+  );
 };
 
 export default reviewComment;
